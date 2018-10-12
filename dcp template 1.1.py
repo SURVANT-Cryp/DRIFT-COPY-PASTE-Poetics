9ada22f4bb0b0a7081aaf3619a3da58ec31552ce
patrick jo quinn <SURVANT-Cryp@users.noreply.github.com>
2018-10-12 18:00:32 -0400

    Add files via upload

--- dcp template 1.1.py
+++ dcp template 1.1.py
@@ -1,17 +1,17 @@
 #! /usr/bin/pythonw
 # coding: utf-8
 
+"""
+cut-up
+"""
+
 import random, textwrap
 from random import randint
-
-"""
-Cut-up
-"""
 
 text = ['do', 'it', 'yourself']
 
 while len(text) > 3:
     text.remove(random.choice(text))
-print "\nDRIFT/COPY/PASTE POETIX:\n\n" + \
+print "\noutput:\n\n" + \
     textwrap.fill(" ".join(text), 40) + random.choice(['']) + '\n'
 
